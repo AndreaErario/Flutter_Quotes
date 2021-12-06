e117908c967d01837bd952bdcc8c0db711bbcd39
AndreaErario <andreaerario04@gmail.com>
2021-12-06 19:36:49 +0100

    Creates placeholder app

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,27 @@ class Quotes extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Quotes"),
+        backgroundColor: Colors.indigo,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Text("A placeholder quote", style: TextStyle(fontSize: 24.0)),
+            Text("~ a placeholder author", style: TextStyle(fontSize: 18.0))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.refresh),
+        backgroundColor: Colors.indigo,
+      ),
+    );
   }
 }
